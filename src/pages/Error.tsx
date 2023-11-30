7ad560c0196490669b02ed2afafbc08473632e9e
Daisuke <d.suke.kida@gmail.com>
2023-12-01 01:06:41 +0900

    ✨ LIFFアプリの開発

---
+++ src/pages/Error.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Error = () => <p>The page is not found.</p>;
+
+export default Error;
---
+++ src/routes/Router.tsx
@@ -0,0 +1,21 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Loading } from "../components/loading";
+
+const Top = lazy(() => import("../pages/Top"));
+const Error = lazy(() => import("../pages/Error"));
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Top />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default Router;

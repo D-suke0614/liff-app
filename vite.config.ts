2c875c6ccd96a8ae31b44c7b7154fb62280c8dd0
Daisuke <d.suke.kida@gmail.com>
2023-12-02 23:02:56 +0900

    build error

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ Object.keys(process.env).forEach((key) => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+    root: './src',
+    build: {
+      outDir: '../out',
+      emptyOutDir: true
+    },
   plugins: [react()],
   define: viteEnv,
 })
